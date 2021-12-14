596c69c3c38dcaca8858775e315a38fd2e383991
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-12-14 19:02:14 +0900

    Add UseEffect function to App.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useEffect } from "react";
+import AudioPlayer from "./AudioPlayer";
 import Header from "./components/Header";
 
 const App = () => {
+
+const audioPlayer = AudioPlayer();
+
+useEffect(() => {
+    audioPlayer.setInstrument("acoustic_grand_piano");
+  }, []);
+
+  const handleClick = () => {
+    audioPlayer.playNote("C4");
+  };
+
   return (
     <div className="App">
       <h1>Message in a Bottle</h1>
+      <button onClick={handleClick}>Play</button>
     </div>
   );
 }
